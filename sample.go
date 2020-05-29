d57197e478d97710d5f8d13ba8f042f38a9b7c99
abhishekb1512 <abhishekb1512@gmail.com>
2020-05-29 17:02:07 +0300

    initial commit

--- sample.go
+++ sample.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func square(x int) int {
+	return x * x
+}
+
 func main() {
-    fmt.Println("Hello Sample")
+
+	var a int = 3
+	c := 6.2
+	d := "she is calling me"
+
+	fmt.Println(a, c, d)
 }
